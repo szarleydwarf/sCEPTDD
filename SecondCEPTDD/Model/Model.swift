a88f0c715fef5255744f344476af40e5a1322811
RJ <r.j.chomik@gmail.com>
2021-01-22 11:35:09 +0000

    initial commit

---
+++ SecondCEPTDD/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  SecondCEPTDD
+//
+//  Created by The App Experts on 22/01/2021.
+//  Copyright © 2021 The App Experts. All rights reserved.
+//
+
+import Foundation
+
+struct Account {
+    
+}
---
+++ SecondCEPTDDTests/SecondCEPTDDTests.swift
@@ -0,0 +1,21 @@
+//
+//  SecondCEPTDDTests.swift
+//  SecondCEPTDDTests
+//
+//  Created by The App Experts on 22/01/2021.
+//  Copyright © 2021 The App Experts. All rights reserved.
+//
+
+import XCTest
+@testable import SecondCEPTDD
+
+class SecondCEPTDDTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+
+}
