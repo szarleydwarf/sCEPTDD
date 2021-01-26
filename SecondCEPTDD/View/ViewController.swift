d57dd8b50a854b0d73229b994de1895114e213cd
RJ <r.j.chomik@gmail.com>
2021-01-26 11:01:06 +0000

    displaying the data in table implemeted

--- SecondCEPTDD/View/ViewController.swift
+++ SecondCEPTDD/View/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    @IBOutlet var vm: ViewModel!
+    @IBOutlet weak var table: UITableView!
+    private var accountsList:[Account]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.table.dataSource = self
+        self.table.delegate = self
+        
+        self.vm = ViewModel()
+        
+        guard let url = JsonFileLoader().getLocalURL(fromFile: "Accounts", withExtension: "json") else {return}
+        guard let data = JsonFileLoader().getData(from: url) else {return}
+        self.vm.setAccountsList(using: data)
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vm?.accountsList.count ?? 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = self.vm?.accountsList[indexPath.row].kind
+        cell.detailTextLabel?.text = "\(self.vm?.accountsList[indexPath.row].balance ?? 0.0)"
+        return cell
+    }
+    
+    
 
 
 }
